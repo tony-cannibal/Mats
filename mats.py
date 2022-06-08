1367ae7c99c0eb18f30d68b28541e49b4d4b695b
tony-cannibal <91645702+tony-cannibal@users.noreply.github.com>
2022-06-08 16:35:36 -0500

    Add files via upload

--- mats.py
+++ mats.py
@@ -11,3 +11,11 @@ def check_file():
     else:
         df = pd.read_csv('C:/Users/YR PROD ORDER/AppData/Materiales/Mayo.csv')
     return df
+
+
+    df_1 = pd.read_excel('C:/Users/YR PROD ORDER/Desktop/MATERIALES/P/M2/m2 2022.06.06.xlsx')
+    df_2 = pd.read_excel('C:/Users/YR PROD ORDER/Desktop/MATERIALES/P/M2/m2 2022.06.07.xlsx')
+
+    concatenated = pd.concat([df_1, df_2], axis=0)
+
+    concatenated.to_excel('C:/Users/YR PROD ORDER/Desktop/MATERIALES/P/M2/concatenated.xlsx', index=False)                         
