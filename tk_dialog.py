7ba59c2204becbd45c5314d95bf541e2d31705ca
tony-cannibal <91645702+tony-cannibal@users.noreply.github.com>
2022-06-10 09:19:32 -0500

    Add files via upload

--- tk_dialog.py
+++ tk_dialog.py
@@ -5,14 +5,21 @@ import pandas as pd
 import datetime
 
 
+# Wee need to use this class to set up variables so that
+# we can return data from functions called from tkinter
+# buttons. This is so we dont use global variables.
+class CustomVarable:
+    def __init__ (self):
+        self.returnedVariable = None
+    def returnVariable (self, x):
+        self.returnedVariable = x
 
 def callback():
-    global data_f
-    
-    filename= fd.askopenfilename(initialdir='C:/Users/YR PROD ORDER/',
-                             title='Open A File',
-                             filetype=(('xlsx files', '*.xlsx'), ('All Files', '*.*'))
-                             ) 
+    filename= fd.askopenfilename(
+        initialdir='C:/Users/YR PROD ORDER/',
+        title='Open A File',
+        filetype=(('xlsx files', '*.xlsx'), ('All Files', '*.*'))
+        ) 
     if filename:
         try:
             filename = r'{}'.format(filename)
@@ -24,24 +31,28 @@ def callback():
     date = datetime.datetime(2022, 6, 8)
     df = df.assign(fecha=date)
     df = df.assign(mes=date.strftime('%B'))
-    data_f = df
+    return df
 
-def save_file():
-    data_f.to_csv('C:/Users/YR PROD ORDER/Documents/test.csv')
+def save_file(data_f):
+    print(data_f.returnedVariable)
+    data_f.returnedVariable.to_csv('C:/Users/YR PROD ORDER/Documents/test.csv')
 
 def button_call():
     save_file(data_f)
 
-
+# Here we are creating an instance of our CustomVariable class
+# so that we can use it later on to get data from button callbacks
+my_variable = CustomVarable()
 
 root = Tk()
 root.geometry('400x400')    
 
+data_f = []
 
-my_button = Button(root, text='Click to Open File', command=callback)
+my_button = Button(root, text='Click to Open File', command=lambda:my_variable.returnVariable(callback()))
 my_button.pack(pady=10)
 
-save_button = Button(root, text='To CSV', command=save_file)
+save_button = Button(root, text='To CSV', command=lambda:save_file(my_variable ))
 save_button.pack()
 
 my_label = Label(root, text='')
